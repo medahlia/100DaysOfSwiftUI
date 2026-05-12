9516db095e121466d6fd213fe28af1f2211cc839
medahlia <matvienko.dash.0k@gmail.com>
2026-05-12 19:45:48 +0300

    init project

---
+++ Project-1-WeSplit/Project-1-WeSplit/Project_1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1_WeSplitApp.swift
+//  Project-1-WeSplit
+//
+//  Created by Home on 12.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Project_1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
