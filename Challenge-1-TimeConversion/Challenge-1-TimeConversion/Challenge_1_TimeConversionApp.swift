7b1e6824094f0f727756cdb9729a5d55884d0803
medahlia <matvienko.dash.0k@gmail.com>
2026-06-01 17:58:20 +0300

    init project

---
+++ Challenge-1-TimeConversion/Challenge-1-TimeConversion/Challenge_1_TimeConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_1_TimeConversionApp.swift
+//  Challenge-1-TimeConversion
+//
+//  Created by Home on 01.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_1_TimeConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge-1-TimeConversion/Challenge-1-TimeConversion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge-1-TimeConversion
+//
+//  Created by Home on 01.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
