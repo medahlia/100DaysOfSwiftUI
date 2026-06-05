f63abe368c550e59b598794cc70e89415edd869f
medahlia <matvienko.dash.0k@gmail.com>
2026-06-05 22:40:50 +0300

    add notes

--- Project-3-ViewsAndModifiers/Project-3-ViewsAndModifiers/ContentView.swift
+++ Project-3-ViewsAndModifiers/Project-3-ViewsAndModifiers/ContentView.swift
@@ -1,14 +1,75 @@
 import SwiftUI
 
-struct ContentView: View {
+struct CapsuleText: View {
+    var text: String
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.blue)
+            .clipShape(.capsule)
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.pink)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
         }
-        .padding()
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            CapsuleText(text: "vvvv")
+            CapsuleText(text: "jjjj")
+            Text("vvvv")
+            Text("aaaa")
+            Text("dddd")
+                .modifier(Title()) // pink
+            Text("kkkk")
+                .titleStyle() // pink
+            
+            Color.indigo
+                .frame(width: 300, height: 200)
+                .watermarked(with: "SwiftUI")
+        }
+        
     }
 }
 
