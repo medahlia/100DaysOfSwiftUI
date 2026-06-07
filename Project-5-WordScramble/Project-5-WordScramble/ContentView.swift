6df48cb29e8cf312530566cc2751ec46e677aaaa
medahlia <matvienko.dash.0k@gmail.com>
2026-06-07 18:06:07 +0300

    add notes

--- Project-5-WordScramble/Project-5-WordScramble/ContentView.swift
+++ Project-5-WordScramble/Project-5-WordScramble/ContentView.swift
@@ -1,17 +1,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+/*
+ func testStrings() {
+     let word = "swift"
+     let checker = UITextChecker()
+     
+     let range = NSRange(location: 0, length: word.utf16.count)
+     let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+     
+     let allGood = misspelledRange.location == NSNotFound
+ }
+ */
