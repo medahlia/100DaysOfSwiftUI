acf82f86753039e831efdf1ee0ead0f5953e1df3
medahlia <matvienko.dash.0k@gmail.com>
2026-06-09 14:30:55 +0300

    add some notes

--- Project-8-Moonshot/Project-8-Moonshot/ContentView.swift
+++ Project-8-Moonshot/Project-8-Moonshot/ContentView.swift
@@ -1,14 +1,19 @@
 import SwiftUI
 
+
 struct ContentView: View {
+    let layout = [
+        GridItem(.adaptive(minimum: 80, maximum: 120))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: layout) {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
+                }
+            }
         }
-        .padding()
     }
 }
 
