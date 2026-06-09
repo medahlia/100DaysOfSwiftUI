fca7ba1f8f9848dbf164f55524daaf38db28938f
medahlia <matvienko.dash.0k@gmail.com>
2026-06-09 16:44:03 +0300

    add missions

--- Project-8-Moonshot/Project-8-Moonshot/ContentView.swift
+++ Project-8-Moonshot/Project-8-Moonshot/ContentView.swift
@@ -2,10 +2,11 @@ import SwiftUI
 
 
 struct ContentView: View {
-    let astronauts = Bundle.main.decode("astronauts.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
     
     var body: some View {
-        Text(String(astronauts.count))
+        Text(String(missions.count))
     }
 }
 
