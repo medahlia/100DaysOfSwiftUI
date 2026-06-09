c927d5fec803531a6446fae5882cd3ab60669362
medahlia <matvienko.dash.0k@gmail.com>
2026-06-09 15:38:09 +0300

    add data

--- Project-8-Moonshot/Project-8-Moonshot/ContentView.swift
+++ Project-8-Moonshot/Project-8-Moonshot/ContentView.swift
@@ -2,17 +2,10 @@ import SwiftUI
 
 
 struct ContentView: View {
-    let layout = [
-        GridItem(.adaptive(minimum: 80, maximum: 120))
-    ]
     
     var body: some View {
         ScrollView(.horizontal) {
-            LazyHGrid(rows: layout) {
-                ForEach(0..<1000) {
-                    Text("Item \($0)")
-                }
-            }
+            
         }
     }
 }
