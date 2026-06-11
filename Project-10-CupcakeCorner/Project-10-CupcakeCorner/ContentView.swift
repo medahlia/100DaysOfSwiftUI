56fd2107c0836a450d3b1ffedc476aef34db56b8
medahlia <matvienko.dash.0k@gmail.com>
2026-06-12 00:52:34 +0300

    add more notes

--- Project-10-CupcakeCorner/Project-10-CupcakeCorner/ContentView.swift
+++ Project-10-CupcakeCorner/Project-10-CupcakeCorner/ContentView.swift
@@ -1,51 +1,26 @@
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
-
-    var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-
-                Text(item.collectionName)
-            }
-        }
-        .task {
-            await loadData()
-        }
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
 
-    func loadData() async {
-        guard let url = URL(
-            string: "https://itunes.apple.com/search?term=taylor+swift&entity=song"
-        ) else {
-            print("Invalid URL")
-            return
-        }
-
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-
-            if let decodedResponse =
-                try? JSONDecoder().decode(Response.self, from: data) {
-
-                results = decodedResponse.results
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
-
-        } catch {
-            print("Invalid data")
+            
+            Section {
+                Button("Create account") {
+                    print("Creating account...")
+                }
+            }
+            .disabled(disableForm)
         }
     }
 }
