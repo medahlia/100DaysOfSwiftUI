4541610ab60b68b087effee0954a8b93578cf7b2
medahlia <matvienko.dash.0k@gmail.com>
2026-06-11 19:46:50 +0300

    upd

---
+++ Project-10-CupcakeCorner/Project-10-CupcakeCorner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project-10-CupcakeCorner
+//
+//  Created by Home on 11.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-10-CupcakeCorner/Project-10-CupcakeCorner/Project_10_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_10_CupcakeCornerApp.swift
+//  Project-10-CupcakeCorner
+//
+//  Created by Home on 11.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Project_10_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
