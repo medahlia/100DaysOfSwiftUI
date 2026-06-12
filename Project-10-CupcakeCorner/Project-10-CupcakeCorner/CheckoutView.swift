6c6c194cb6169bc48f7d9cab96634e659eb09712
medahlia <matvienko.dash.0k@gmail.com>
2026-06-12 16:06:41 +0300

    add AddressView

---
+++ Project-10-CupcakeCorner/Project-10-CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
