0e6d639c35ba45b0dd3e0577db2fc77145c74d16
medahlia <matvienko.dash.0k@gmail.com>
2026-06-14 21:23:16 +0300

    upd

--- Project-10-CupcakeCorner/Project-10-CupcakeCorner/Order.swift
+++ Project-10-CupcakeCorner/Project-10-CupcakeCorner/Order.swift
@@ -30,4 +30,24 @@ class Order {
         
         return true
     }
+    
+    var cost: Decimal {
+        // $2 per cake
+        var cost = Decimal(quantity) * 2
+        
+        // compicated cakes cost more
+        cost += Decimal(type) / 2
+        
+        // $1/cake for extra frosting
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+        
+        // $0.50/cake for sprinkles
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+        
+        return cost
+    }
 }
