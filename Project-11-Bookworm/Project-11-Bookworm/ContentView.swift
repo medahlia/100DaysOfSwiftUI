db166cb1ee79f67abda8b19fef76fa5a11e01899
medahlia <matvienko.dash.0k@gmail.com>
2026-06-15 10:39:35 +0300

    add Book, AddBookView

--- Project-11-Bookworm/Project-11-Bookworm/ContentView.swift
+++ Project-11-Bookworm/Project-11-Bookworm/ContentView.swift
@@ -1,9 +1,27 @@
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    
+    @State private var showingAddScreen = false
     
     var body: some View {
-        
+        NavigationStack {
+            Text("Count: \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Add Book", systemImage: "plus") {
+                            showingAddScreen.toggle()
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                }
+        }
     }
 }
 
