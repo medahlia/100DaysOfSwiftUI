45b3986a78be5be5a79352e5ca0e8eea768a3b8d
medahlia <matvienko.dash.0k@gmail.com>
2026-06-19 00:15:32 +0300

    init project

---
+++ Project-14-BucketList/Project-14-BucketList/Project_14_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_14_BucketListApp.swift
+//  Project-14-BucketList
+//
+//  Created by Home on 18.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Project_14_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
