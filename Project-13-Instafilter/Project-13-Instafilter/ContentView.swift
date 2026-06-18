f113686a3208bda8d1047e314b5f025fe05e44ea
medahlia <matvienko.dash.0k@gmail.com>
2026-06-18 11:54:41 +0300

    update notes

--- Project-13-Instafilter/Project-13-Instafilter/ContentView.swift
+++ Project-13-Instafilter/Project-13-Instafilter/ContentView.swift
@@ -1,34 +1,12 @@
-import PhotosUI
+import StoreKit
 import SwiftUI
 
 struct ContentView: View {
-    @State private var pickerItems = [PhotosPickerItem]()
-    @State private var selectedImages = [Image]()
+    @Environment(\.requestReview) var requestReview
     
     var body: some View {
-        VStack {
-            PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)])) {
-                Label("Select a picture", systemImage: "photo")
-            }
-            
-            ScrollView {
-                ForEach(0..<selectedImages.count, id: \.self) { i in
-                    selectedImages[i]
-                        .resizable()
-                        .scaledToFit()
-                }
-            }
-        }
-        .onChange(of: pickerItems) {
-            Task {
-                selectedImages.removeAll()
-                
-                for item in pickerItems {
-                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
-                        selectedImages.append(loadedImage)
-                    }
-                }
-            }
+        Button("Leave a review") {
+            requestReview()
         }
     }
 }
