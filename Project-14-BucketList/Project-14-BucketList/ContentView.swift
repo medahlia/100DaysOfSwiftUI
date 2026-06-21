05447f19997c0def6e98b3d4bca3473aab9eec90
medahlia <matvienko.dash.0k@gmail.com>
2026-06-21 05:35:23 +0300

    update

--- Project-14-BucketList/Project-14-BucketList/ContentView.swift
+++ Project-14-BucketList/Project-14-BucketList/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  Project-14-BucketList
-//
-//  Created by Home on 18.06.2026.
-//
-
 import SwiftUI
 
 struct ContentView: View {
--- Project-14-BucketList/Project-14-BucketList/Project_14_BucketListApp.swift
+++ Project-14-BucketList/Project-14-BucketList/Project_14_BucketListApp.swift
@@ -1,10 +1,3 @@
-//
-//  Project_14_BucketListApp.swift
-//  Project-14-BucketList
-//
-//  Created by Home on 18.06.2026.
-//
-
 import SwiftUI
 
 @main
