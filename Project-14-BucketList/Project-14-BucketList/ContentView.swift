345ae3967a07f0d12986df6d9ce4d98dc625bd0e
medahlia <matvienko.dash.0k@gmail.com>
2026-06-22 23:52:01 +0300

    update

--- Project-14-BucketList/Project-14-BucketList/ContentView.swift
+++ Project-14-BucketList/Project-14-BucketList/ContentView.swift
@@ -1,14 +1,24 @@
 import SwiftUI
 
+struct User: Identifiable {
+    let id = UUID()
+    var firstName: String
+    var lastName: String
+}
+
 struct ContentView: View {
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister")
+    ].sorted {
+        $0.lastName < $1.lastName
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(users) { user in
+            Text("\(user.firstName) \(user.lastName)")
         }
-        .padding()
     }
 }
 
