857afd38e674f0edb5590f74967d6bb046b42aa5
medahlia <matvienko.dash.0k@gmail.com>
2026-06-23 19:06:52 +0300

    add notes

--- Project-14-BucketList/Project-14-BucketList/ContentView.swift
+++ Project-14-BucketList/Project-14-BucketList/ContentView.swift
@@ -1,23 +1,38 @@
 import SwiftUI
 
-struct User: Identifiable {
-    let id = UUID()
-    var firstName: String
-    var lastName: String
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
 }
 
 struct ContentView: View {
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Kristine", lastName: "Kochanski"),
-        User(firstName: "David", lastName: "Lister")
-    ].sorted {
-        $0.lastName < $1.lastName
+    enum LoadingState {
+        case loading, success, failed
     }
     
+    @State private var loadingState = LoadingState.loading
+    
     var body: some View {
-        List(users) { user in
-            Text("\(user.firstName) \(user.lastName)")
+        switch loadingState {
+        case .loading:
+            LoadingView()
+        case .success:
+            SuccessView()
+        case .failed:
+            FailedView()
         }
     }
 }
@@ -25,3 +40,18 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+/*
+ Button("Read and Write") {
+             let data = Data("Test Message".utf8)
+             let url = URL.documentsDirectory.appending(path: "message.txt")
+             
+             do {
+                 try data.write(to: url, options: [.atomic, .completeFileProtection])
+                 let input = try String(contentsOf: url, encoding: .utf8)
+                 print(input)
+             } catch {
+                 print(error.localizedDescription)
+             }
+         }
+ */
