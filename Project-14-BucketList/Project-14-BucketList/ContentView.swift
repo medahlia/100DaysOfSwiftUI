321e71e5cad3de8a0e2e2c9a6af2f078350ba487
medahlia <matvienko.dash.0k@gmail.com>
2026-06-23 21:23:50 +0300

    add notes (LocalAuthentication)

--- Project-14-BucketList/Project-14-BucketList/ContentView.swift
+++ Project-14-BucketList/Project-14-BucketList/ContentView.swift
@@ -1,42 +1,36 @@
-import MapKit
+import LocalAuthentication
 import SwiftUI
 
-struct Location: Identifiable {
-    let id = UUID()
-    var name: String
-    var coordinate: CLLocationCoordinate2D
-}
-
 struct ContentView: View {
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
+    @State private var isUnlocked = false
     
     var body: some View {
         VStack {
-            MapReader { proxy in
-//                Map {
-//                    ForEach(locations) { location in
-//                        Annotation(location.name, coordinate: location.coordinate) {
-//                            Text(location.name)
-//                                .font(.headline)
-//                                .padding()
-//                                .background(.blue.gradient)
-//                                .foregroundStyle(.white)
-//                                .clipShape(.capsule)
-//                        }
-//                        .annotationTitles(.hidden)
-//                    }
-//                }
-                
-                Map()
-                    .onTapGesture { position in
-                        if let coordinate = proxy.convert(position, from: .local) {
-                            print(coordinate)
-                        }
-                    }
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
             }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
         }
     }
 }
