b261ea7d62116be103d03ee286eece4183ee8fe2
medahlia <matvienko.dash.0k@gmail.com>
2026-06-27 14:37:43 +0300

    test upd

--- Project-16-HotProspects/Project-16-HotProspects/ContentView.swift
+++ Project-16-HotProspects/Project-16-HotProspects/ContentView.swift
@@ -1,39 +1,9 @@
 import SwiftUI
-import UserNotifications
 
 struct ContentView: View {
     
     var body: some View {
-        VStack {
-            Button("Request Permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                    if success {
-                        print("All set!")
-                    } else if let error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            
-            Button("Schedule Notification") {
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the cat"
-                content.subtitle = "It looks hungry"
-                content.sound = UNNotificationSound.default
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request) { error in
-                    if let error {
-                        print(error.localizedDescription)
-                    } else {
-                        print("Notification scheduled")
-                    }
-                }
-            }
-        }
+        
     }
 }
 
