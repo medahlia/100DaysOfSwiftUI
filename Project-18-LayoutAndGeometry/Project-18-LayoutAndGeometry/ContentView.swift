490ef05d867a1027f3533818b118b36b20ab8f80
medahlia <matvienko.dash.0k@gmail.com>
2026-07-01 10:33:22 +0300

    add notes about custom alignment guide

--- Project-18-LayoutAndGeometry/Project-18-LayoutAndGeometry/ContentView.swift
+++ Project-18-LayoutAndGeometry/Project-18-LayoutAndGeometry/ContentView.swift
@@ -1,14 +1,38 @@
 import SwiftUI
 
+extension VerticalAlignment {
+    struct MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+    
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@sheldon")
+                    .alignmentGuide(.midAccountAndName) { d in
+                        d[VerticalAlignment.center]
+                    }
+                
+                Image(.sheldonCooper)
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+            
+            VStack {
+                Text("Full name:")
+                Text("SHELDON COOPER")
+                    .alignmentGuide(.midAccountAndName, computeValue: { d in
+                        d[VerticalAlignment.center]
+                    })
+                    .font(.largeTitle)
+            }
         }
-        .padding()
     }
 }
 
